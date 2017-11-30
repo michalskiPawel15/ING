5ae4394a0bc133e6f0fd4363658b1dbb202cc584
michalskiPawel15 <michalski.pawel.15@gmail.com>
2017-11-30 01:10:35 +0100

    Weather App

--- modules/ajax_module.js
+++ modules/ajax_module.js
@@ -11,9 +11,13 @@ var appCtrl=function($scope,$http){
 	*/
 	var citiesUrl='cities.json';
 	$http.get(citiesUrl).then(function(response){
-		$scope.cities=response.data;
 		//Success response
 		//debug(response.data,'---Success---');
+		$scope.cities=response.data;
+		angular.forEach($scope.cities,function(city){
+			city.show=false;
+			city.refresh=0;
+		});
 	},function(response){
 		//Error response
 		debug(response,'---Error---');
@@ -22,7 +26,6 @@ var appCtrl=function($scope,$http){
 		Get weather
 	*/
 	var getWeather=function(city){
-		// var jsonUrl='http://danepubliczne.imgw.pl/api/data/synop';
 		var appId='ef27a31b33920f047388689eed3f8cf2';
 		var weatherUrl='http://api.openweathermap.org/data/2.5/weather?id='+city.id+'&units=metric&lang=pl&APPID='+appId;
 		$http.get(weatherUrl).then(function(response){
@@ -36,6 +39,15 @@ var appCtrl=function($scope,$http){
 			debug(response,'---Error---');
 		});
 	};
+	var setPrevVal=function(val){
+		var prevVal=val;
+		if(typeof(prevVal)==="undefined"){
+			return val=1;
+		}
+		else{
+			return val=prevVal;
+		}
+	};
 	$scope.checkAll=function(){
 		if($scope.allCheck){
 			$scope.allCheck=true;
@@ -48,6 +60,7 @@ var appCtrl=function($scope,$http){
 			if($scope.allCheck){
 				getWeather(city);
 				city.show=true;
+				city.refresh=setPrevVal(city.refresh);
 			}
 			else{
 				city.show=false;
@@ -56,13 +69,21 @@ var appCtrl=function($scope,$http){
 	};
 	$scope.checkCity=function(city){
 		if(city.selected){
-			// $scope.allCheck=true;
 			getWeather(city);
 			city.show=true;
+			city.refresh=setPrevVal(city.refresh);
 		}
 		else{
 			$scope.allCheck=false;
 			city.show=false;
+		}
+	};
+	$scope.test=function(refVal){
+		if(refVal===null){
+			this.city.refresh=1;
+		}
+		else if(typeof(refVal)==="undefined"){
+			this.city.refresh=60;
 		}
 	};
 };
